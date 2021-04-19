519cb3eb5bb10f813885284a10f0c94b22b16565
Taiyr Begeyev <taiyrbegeyev@gmail.com>
2021-04-19 15:40:34 +0200

    :sparkles: add hooks for hashing passwords using bcrypt with salt and compare passwords

--- server/src/models/moderators.ts
+++ server/src/models/moderators.ts
@@ -1,3 +1,4 @@
+import * as bcrypt from 'bcrypt';
 import * as mongoose from 'mongoose';
 
 export enum Role {
@@ -9,25 +10,27 @@ export enum Role {
   InfectionReportManager = 'InfectionReportManager'
 }
 
-export interface IModerator {
-  email: String;
-  password: String;
-  salt: String;
-  firstName: String;
-  lastName: String;
+export interface IModerator extends mongoose.Document {
+  email: string;
+  hash: string;
+  firstName: string;
+  lastName: string;
   roles: Array<Role>;
   isRegistered: Boolean;
   invitationDate: Date;
   registrationDate: Date;
+  comparePassword: (password: string) => Promise<Boolean>;
 }
 
-const moderatorSchema: mongoose.Schema = new mongoose.Schema({
+const moderatorSchema = new mongoose.Schema<IModerator>({
   email: {
     type: String,
     unique: true
   },
-  password: String,
-  salt: String,
+  hash: {
+    type: String,
+    select: false // Omit the hash when returning a moderator
+  },
   firstName: String,
   lastName: String,
   roles: {
@@ -39,6 +42,39 @@ const moderatorSchema: mongoose.Schema = new mongoose.Schema({
   invitationDate: { type: Date, default: Date.now },
   registrationDate: Date
 });
+
+/**
+ * Password hash middleware.
+ */
+// coderrocketfuel.com/article/store-passwords-in-mongodb-with-node-js-mongoose-and-bcrypt
+// github.com/microsoft/TypeScript-Node-Starter/blob/master/src/models/User.ts
+moderatorSchema.pre<IModerator>('save', function save(next) {
+  // eslint-disable-next-line @typescript-eslint/no-this-alias
+  const user = this;
+  if (this.isModified('hash') || this.isNew) {
+    return bcrypt.genSalt(10, (saltError, salt) => {
+      if (saltError) {
+        return next(saltError);
+      }
+      return bcrypt.hash(user.hash, salt, (hashError, hash) => {
+        if (hashError) {
+          return next(hashError);
+        }
+
+        user.hash = hash;
+        return next();
+      });
+    });
+  }
+  return next();
+});
+
+moderatorSchema.methods.comparePassword = async function (
+  password: string
+): Promise<Boolean> {
+  const isPasswordMatching: Boolean = await bcrypt.compare(password, this.hash);
+  return isPasswordMatching;
+};
 
 export const moderatorModel = mongoose.model<IModerator & mongoose.Document>(
   'Moderator',
