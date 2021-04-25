9847909f3d457d10314a7bcb059d1dda0263e18f
Taiyr Begeyev <taiyrbegeyev@gmail.com>
2021-04-25 14:31:04 +0200

    :sparkles: add a condition to the phone number and zipcode

--- server/src/validators/checkIn_validator.ts
+++ server/src/validators/checkIn_validator.ts
@@ -1,17 +1,29 @@
-import Joi from 'joi';
+import config from 'config';
+import Joi, { optional, required } from 'joi';
 
 class CheckInValidator {
   checkInSchema = Joi.object({
-    eventID: Joi.string().required(),
+    eventId: Joi.string().required(),
+    eventDuration: Joi.number()
+      .integer()
+      .positive()
+      .max(config.event.maxDuration)
+      .required(),
     email: Joi.string()
       .email({ tlds: { allow: false } })
       .trim()
       .required(),
     isGuest: Joi.boolean().required(),
-    phoneNumber: Joi.string()
-      .phoneNumber({ format: 'international' })
-      .required(),
-    zipCode: Joi.string().required()
+    phoneNumber: Joi.string().when('isGuest', {
+      is: true,
+      then: required(),
+      otherwise: optional()
+    }),
+    zipCode: Joi.string().when('isGuest', {
+      is: true,
+      then: required(),
+      otherwise: optional()
+    })
   });
 }
 
