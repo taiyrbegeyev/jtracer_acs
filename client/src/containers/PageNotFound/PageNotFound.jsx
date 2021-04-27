061c4a8bcffd3ffc877433f2c89378b0ae92a38e
Taiyr Begeyev <taiyrbegeyev@gmail.com>
2021-04-27 21:28:15 +0200

    :sparkles: introduce 404 page

--- client/src/containers/PageNotFound/PageNotFound.jsx
+++ client/src/containers/PageNotFound/PageNotFound.jsx
@@ -1,23 +1,39 @@
-import { Grid } from '@material-ui/core';
+import { Box, Grid, Typography } from '@material-ui/core';
 import React from 'react';
 import { withNamespaces } from 'react-i18next';
-import notFoundIllustrations from 'assets/illustrations/undraw_page_not_found.svg';
+import notFoundIllustration from 'assets/illustrations/undraw_page_not_found.svg';
+import { NotFoundIllustration } from './styles';
 
 const PageNotFound = () => {
   return (
     <Grid
       container
-      spacing={0}
+      spacing={4}
       alignItems="center"
       justify="center"
-      direction="row"
+      direction="column"
       style={{ minHeight: '100vh' }}
     >
+      <Grid item xs={4}>
+        <Box mb={1}>
+          <Typography variant="h4" align="center">
+            Oops! Page not found
+          </Typography>
+        </Box>
+        <Box mb={1}>
+          <Typography variant="subtitle1" align="center">
+            Sorry, but the page you are looking for is not found. Please, make
+            sure that you have typed the current URL
+          </Typography>
+        </Box>
+        <Box>
+          <Typography variant="subtitle2" align="center">
+            Home Page
+          </Typography>
+        </Box>
+      </Grid>
       <Grid item xs={3}>
-        <img
-          src={notFoundIllustrations}
-          style={{ maxWidth: '100%', maxHeight: '100%' }}
-        />
+        <NotFoundIllustration src={notFoundIllustration} />
       </Grid>
     </Grid>
   );
